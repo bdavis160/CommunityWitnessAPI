95c6779cca4106040a44ea9f8833536a5662cbcf
Kyle Zalewski <kyle.zalewski@seeq.com>
2021-07-20 20:59:13 -0700

    Added files for tests on main (non-rel) table-related classes. They're pretty empty as of now as general consensus seems to be simple mutators don't need to be unit tested. The files are there for whatever we decide we actually do need to test.

---
+++ Backend/src/test/java/org/CommunityWitness/Backend/InvestigatorTest.java
@@ -0,0 +1,9 @@
+package org.CommunityWitness.Backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvestigatorTest {
+
+}
---
+++ Backend/src/test/java/org/CommunityWitness/Backend/WitnessTest.java
@@ -0,0 +1,12 @@
+package org.CommunityWitness.Backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WitnessTest {
+
+    @Test
+    void updateFrom() {
+    }
+}
