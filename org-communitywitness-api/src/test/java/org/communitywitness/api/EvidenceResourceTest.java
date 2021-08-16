0deb7d5f7d119915e189fda6711a76b5a5c1362c
Kyle Zalewski <kaz4@pdx.edu>
2021-08-15 18:41:31 -0700

    added a unit test

--- org-communitywitness-api/src/test/java/org/communitywitness/api/EvidenceResourceTest.java
+++ org-communitywitness-api/src/test/java/org/communitywitness/api/EvidenceResourceTest.java
@@ -2,13 +2,35 @@ package org.communitywitness.api;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class EvidenceResourceTest {
     EvidenceResource res = new EvidenceResource();
 
     @Test
-    void createEvidence() {
+    void createEvidence() throws IOException, SQLException {
+        String location = "C:\\Users\\Kyle\\IdeaProjects\\CommunityWitnessAPI\\org-communitywitness-api\\test.jpg";
+        Path path = Paths.get(location);
+        String data = Base64.getEncoder().encodeToString(Files.readAllBytes(path));
+
+        NewEvidenceRequest req = new NewEvidenceRequest();
+        req.setTitle("From unit test");
+        req.setType("image");
+        req.setTimestamp(LocalDateTime.now());
+        req.setReportId(2);
+        req.setData(data);
+
+        int id = res.createEvidence(req);
+        assertNotEquals(-1, id);
     }
 
     @Test
